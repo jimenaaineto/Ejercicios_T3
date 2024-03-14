e06ac39b8726485570e647d45243609bc6d0df63
jimenaaineto <159237388+jimenaaineto@users.noreply.github.com>
2024-03-14 14:40:29 +0000

    Ejercicio2

---
+++ Ejercicio2/Main2.java
@@ -0,0 +1,13 @@
+package Ejercicio2;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        Fecha fecha=new Fecha(20,10,2018);
+        System.out.println(fecha.toString());
+         fecha.setAño(2019);
+         System.out.println(fecha.toString());
+
+    }
+    
+}
